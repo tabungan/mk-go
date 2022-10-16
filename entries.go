7aa4a3ee6e75424cf1f96174747db926d723baf4
tabungan <115408559+tabungan@users.noreply.github.com>
2022-10-17 06:25:27 +0700

    perf: parallelizes entries scaffolding

--- entries.go
+++ entries.go
@@ -1,12 +1,58 @@
 package mk
 
+import "sync"
+
+type entriesRunner struct {
+	errChan chan error
+	wg      sync.WaitGroup
+}
+
+func initEntriesRunner() entriesRunner {
+	return entriesRunner{
+		errChan: make(chan error),
+	}
+}
+
+func (runner *entriesRunner) scheduleProcessing(basePath string, entry Entry) {
+	runner.wg.Add(1)
+
+	go (func() {
+		defer runner.wg.Done()
+
+		if err := entry.Scaffold(basePath); err != nil {
+			runner.errChan <- err
+		}
+	})()
+}
+
+func (runner *entriesRunner) unblockErrorChannelOnCompletion() {
+	go (func() {
+		runner.wg.Wait()
+
+		runner.errChan <- nil
+		close(runner.errChan)
+	})()
+}
+
+func (runner *entriesRunner) fetchFirstErrorAndDrainOthers() error {
+	firstErr := <-runner.errChan
+
+	go (func() {
+		for range runner.errChan {
+		}
+	})()
+	return firstErr
+}
+
 type Entries []Entry
 
 func (entries Entries) Scaffold(basePath string) error {
+	runner := initEntriesRunner()
+
 	for _, entry := range entries {
-		if err := entry.Scaffold(basePath); err != nil {
-			return err
-		}
+		runner.scheduleProcessing(basePath, entry)
 	}
-	return nil
+	runner.unblockErrorChannelOnCompletion()
+
+	return runner.fetchFirstErrorAndDrainOthers()
 }
