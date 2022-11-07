10381361b3b0388ff345482db8a8dbaaf4c962db
tabungan <115408559+tabungan@users.noreply.github.com>
2022-11-07 23:40:09 +0700

    docs: describe Dir

--- dir.go
+++ dir.go
@@ -5,12 +5,18 @@ import (
 	"path/filepath"
 )
 
+// Type Dir is a building block for scaffolding a directory.
+//
+// These parameters can be set to customize the scaffolding process:
+//   - Name specifies the directory name.
+//   - Content specifies the scaffolding to be scaffolded on the top of the directory.
 type Dir struct {
 	Name string
 
 	Content Entry
 }
 
+// Scaffold scaffolds a directory on the top of basePath.
 func (dir Dir) Scaffold(basePath string) error {
 	path := filepath.Join(basePath, dir.Name)
 
