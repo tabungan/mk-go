d3b1379cb1124453038bf5cfbd45237926e00518
tabungan <115408559+tabungan@users.noreply.github.com>
2022-11-08 00:20:22 +0700

    docs: describe Scaffold, ScaffoldOrPanic

--- scaffold.go
+++ scaffold.go
@@ -1,9 +1,15 @@
 package mk
 
+// Scaffold scaffolds entry on the top of basePath.
+//
+// An error will be returned if the scaffolding process fails, otherwise
+// nil will be returned.
 func Scaffold(basePath string, entry Entry) error {
 	return entry.Scaffold(basePath)
 }
 
+// ScaffoldOrPanic scaffolds entry on the top of basePath, or
+// panic if the scaffolding process fails.
 func ScaffoldOrPanic(basePath string, entry Entry) {
 	if err := Scaffold(basePath, entry); err != nil {
 		panic(err)
