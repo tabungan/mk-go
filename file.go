38c46847dc0386397b37da47e9845df7a0faa069
tabungan <115408559+tabungan@users.noreply.github.com>
2022-11-07 23:21:23 +0700

    docs: describe File

--- file.go
+++ file.go
@@ -5,12 +5,18 @@ import (
 	"path/filepath"
 )
 
+// Type File is a building block for scaffolding a file.
+//
+// These parameters can be set to customize the scaffolding process:
+//   - Name specifies the file name.
+//   - Content specifies the file content in []byte.
 type File struct {
 	Name string
 
 	Content []byte
 }
 
+// Scaffold scaffolds a file on the top of basePath.
 func (file File) Scaffold(basePath string) error {
 	return os.WriteFile(filepath.Join(basePath, file.Name), file.Content, 0600)
 }
